1685a914729a6bf4cf627a5808f8e20ae6f6e90a
Diego Ramires <goncalves.ramires@gmail.com>
2017-10-14 14:29:26 -0300

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'friendly_id', '~> 5.2', '>= 5.2.1'
 gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.alpha6'
+
+gem 'name_view_tool', git: 'https://github.com/DiegoRamires/name_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    NameViewTool::Renderer.copyright 'Diego Ramires', 'All rights reserved'
+  end
 end
